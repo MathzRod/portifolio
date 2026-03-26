617f0b047dc97aadafa5876cac3fd86c70f29628
Matheus Rodrigues de Camargo <mathzrodrsocial@gmail.com>
2026-03-26 20:16:40 -0300

    V0.3 - Implemented Form Layout

--- src/components/layout/Form.jsx
+++ src/components/layout/Form.jsx
@@ -1,13 +1,33 @@
-
-
-export default function Form(){
-  return(
+export default function Form() {
+  return (
     <section className="w-full mt-7 flex flex-col items-center gap-4 px-5">
       <h2 className=" text-center text-white text-4xl font-bold mb-4 w-45">
         Let's Work <span className="text-(--color-text-span)">Together</span>
       </h2>
-      <form action="">
+      <form action="" className="flex flex-col gap-5 w-full">
 
+        {/* NOME */}
+        <div className="flex flex-nowrap gap-2.5 w-full ">
+          <div className="flex flex-col w-1/2 min-w-0">
+            <label htmlFor="name" className="text-(--color-text-p)">Nome</label>
+            <input type="text" id="name" name="name" placeholder="Digite seu nome..."  className="bg-(--background-area) rounded-lg p-3 text-(--color-placeholder)"/>
+          </div>
+
+        {/* EMAIL  */}
+          <div className="flex flex-col w-1/2 min-w-0">
+            <label htmlFor="email" className="text-(--color-text-p)" >E-mail</label>
+            <input type="email" id="email" name="email" placeholder="Digite seu e-mail..." className="bg-(--background-area) rounded-lg p-3 text-(--color-placeholder)"/>
+          </div>
+        </div>
+
+        {/* MENSAGEM  */}
+        <div className="flex flex-col ">
+          <label htmlFor="message" className="text-(--color-text-p)">Message</label>
+          <textarea id="message" name="message" placeholder="Message" rows={5}  className="bg-(--background-area) rounded-lg p-3 text-(--color-placeholder)"/>
+        </div>
+         
+         {/* BOTAO */}
+        <button type="submit" className="w-full py-2 rounded-lg bg-(--color-highlight) text-(--color-text) font-bold">Submit</button>
 
       </form>
 
--- src/components/layout/Main.jsx
+++ src/components/layout/Main.jsx
@@ -15,6 +15,7 @@ export default function Main() {
       <Projects />
       <Experience />
       <Form/>
+      <Footer/>
     </main>
   )
 }
